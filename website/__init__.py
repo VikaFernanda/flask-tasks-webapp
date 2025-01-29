55b28eda8eafe3c43cbf52f7a33c60095f887e27
vika-fernanda <tlmaior@gmail.com>
2025-01-29 16:41:39 -0300

    RDS implementation

--- website/__init__.py
+++ website/__init__.py
@@ -6,12 +6,12 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
-DB_NAME = "database.db"
+# DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'fjgsftzrdjovzuey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://demo_admin:m#xyb!4q<$WZV_w6W2QD]7Z3GbQ0@demo-env-db.cpxgsuaa8wqm.us-west-1.rds.amazonaws.com:5432/demo_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://demo_admin:m#xyb!4q<$WZV_w6W2QD]7Z3GbQ0@demo-env-db.cpxgsuaa8wqm.us-west-1.rds.amazonaws.com:5432/demo-env-db'
     db.init_app(app)
     migrate.init_app(app, db)
     
