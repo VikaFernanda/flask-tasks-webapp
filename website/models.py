742e8921ab6695330457dabbb3cc608b1d5f2bcc
Vika_Fernanda <98906257+VikaFernanda@users.noreply.github.com>
2025-01-30 17:36:26 -0300

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,17 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
+from . import db
 from flask_login import UserMixin
-
-db = SQLAlchemy()
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
-    tasks = db.relationship('Task', backref='owner', lazy=True)
+from sqlalchemy.sql import func
 
 class Task(db.Model):
+    __tablename__ = "tasks"
+
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    completed = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    is_done = db.Column(db.Boolean, default=False)
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users" 
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(700), nullable=False)
+    first_name = db.Column(db.String(150), nullable=False)
+    tasks = db.relationship('Task', backref='owner', lazy=True) 
