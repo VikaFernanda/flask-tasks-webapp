56fed73aebfbdaf9f2d397af1f3180b9c09cbb26
VikaFernanda <victoria.ashtonex@outlook.com>
2025-02-04 17:49:36 -0300

    config file changes

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-# import os
-# from dotenv import load_dotenv
-from urllib.parse import quote_plus
+import os
+from dotenv import load_dotenv
 
-# # Load environment variables
-# load_dotenv()
+# Load environment variables
+load_dotenv()
 
 class Config:
-    SECRET_KEY = '1b1a0de2fa29ad0844a4a92dfbca7d1af2132909ef02f4549f6dba6292f5e071'
-    PASSWORD = quote_plus("m#xyb!4q<$WZV_w6W2QD]7Z3GbQ0")  # Encode special characters
-    SQLALCHEMY_DATABASE_URI = f'postgresql://demo_admin:{PASSWORD}@demo-env-db.cpxgsuaa8wqm.us-west-1.rds.amazonaws.com/demodb'
+    SECRET_KEY = os.getenv('SECRET_KEY', '1b1a0de2fa29ad0844a4a92dfbca7d1af2132909ef02f4549f6dba6292f5e071')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://demo_admin:m#xyb!4q<$WZV_w6W2QD]7Z3GbQ0@demo-env-db.cpxgsuaa8wqm.us-west-1.rds.amazonaws.com/demodb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
