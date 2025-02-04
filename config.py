fa22a3e9f7aa8ea9e60fb1d48646b015a3309c3c
VikaFernanda <victoria.ashtonex@outlook.com>
2025-02-04 17:37:37 -0300

    config.y changes

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
-import os
-from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-# Load environment variables
-load_dotenv()
-
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', '1b1a0de2fa29ad0844a4a92dfbca7d1af2132909ef02f4549f6dba6292f5e071')
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://vika-admin:wRuRUEaRM53MMhPG@flaskdb.ighkt.mongodb.net/?retryWrites=true&w=majority&appName=flaskdb')
+    SECRET_KEY = '1b1a0de2fa29ad0844a4a92dfbca7d1af2132909ef02f4549f6dba6292f5e071'
+    MONGO_URI = 'mongodb+srv://vika-admin:wRuRUEaRM53MMhPG@flaskdb.ighkt.mongodb.net/?retryWrites=true&w=majority&appName=flaskdb'
     
 # Create a new client and connect to the server
 client = MongoClient(Config.MONGO_URI, server_api=ServerApi('1'))
