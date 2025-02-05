8022ad7b2c1003f13233728b5b15eb71ab09cf04
VikaFernanda <victoria.ashtonex@outlook.com>
2025-02-05 14:04:44 -0300

    config file correction

--- config.py
+++ config.py
@@ -6,5 +6,5 @@
 
 class Config:
     SECRET_KEY = '1b1a0de2fa29ad0844a4a92dfbca7d1af2132909ef02f4549f6dba6292f5e071'
-    SQLALCHEMY_DATABASE_URI = 'DATABASE_URL', 'postgresql://flask_admin:yGmgKJ!7MVG9gjQq@flaskwebapp.cpxgsuaa8wqm.us-west-1.rds.amazonaws.com/flaskdb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_admin:yGmgKJ!7MVG9gjQq@flaskwebapp.cpxgsuaa8wqm.us-west-1.rds.amazonaws.com/flaskdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
