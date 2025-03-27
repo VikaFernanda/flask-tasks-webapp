010600c5509b4a4fb4d2c0b1770a1d1feccef961
CRFLuiz <lcmartinscjr@gmail.com>
2025-03-27 20:15:49 +0000

    update app to use env vars

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-# import os
+import os
 # from dotenv import load_dotenv
 
 # # Load environment variables
 # load_dotenv()
 
 class Config:
-    SECRET_KEY = '1b1a0de2fa29ad0844a4a92dfbca7d1af2132909ef02f4549f6dba6292f5e071'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_admin:yGmgKJ!7MVG9gjQq@dev-demo-env-db.cpxgsuaa8wqm.us-west-1.rds.amazonaws.com/flaskdb'
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
